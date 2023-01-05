bfbe862487992c85159a4a1aaed17a8ac7732202
Lindsey Archipley <lindsey.archipley@gmail.com>
2023-01-05 14:29:49 -0500

    Write subtract & multiply functions

--- javascript.js
+++ javascript.js
@@ -4,5 +4,16 @@ const add = function(a, b) {
     return a + b;
 };
 
-add(3,5); //TEST
+// add(3,5);
 
+const subtract = function(a, b) {
+    return a - b;
+};
+
+// subtract(10,4); 
+
+const multiply = function(a, b) {
+    return a * b;
+};
+
+// multiply(4,5);
