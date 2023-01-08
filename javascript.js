783a07e8c44c87674a5bc98ad00b9d8a89fd47ec
Lindsey Archipley <lindsey.archipley@gmail.com>
2023-01-08 13:02:37 -0500

    Add click event listeners to buttons

--- javascript.js
+++ javascript.js
@@ -40,6 +40,33 @@ const operate = function(operator, a, b) {
 
 operate('.', 10, 2);
 
+
+//when a button is pressed, display that input to user (press 2, display 2)
+//when another button is pressed, add that input to the current input display (press 4, display '24', press +, 'display 24 +' and so on)
+//make clear button clear values and display
+
+//the below code adds event listeners to each button & calls the "displayCurrentValue" function on click
+
+let current;
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        current = button.value;
+        displayCurrentValue();
+})});
+
+//the below code populates a 'running display' that shows the value of the button most recently clicked
+
+const runningDisplay = document.querySelector('.running-display');
+let displayCurrentValue = function() {
+runningDisplay.innerHTML = "";
+const currentValue = document.createElement('h3');
+currentValue.textContent = `${current}`;
+runningDisplay.appendChild(currentValue);
+};
+
 //the below code populates the final result on the screen
 
 const display = document.querySelector('.display');
@@ -48,6 +75,4 @@ displayValue.textContent = `EQUALS ${result}`;
 display.appendChild(displayValue);
 
 
-//when a button is pressed, display that input to user (press 2, display 2)
-//when another button is pressed, add that input to the current input display (press 4, display '24', press +, 'display 24 +' and so on)
-//make clear button clear values and display
+
